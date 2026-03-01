923f57a30a9483ce273bfb35e215876fc83f1e8f
SATENDRA KUMAR YADAV <princeworks990@gmail.com>
2026-03-01 15:23:10 +0530

    Refactor Message schema content field formatting

--- Backend/models/Message.js
+++ Backend/models/Message.js
@@ -1,4 +1,3 @@
-// backend/models/Message.js
 const mongoose = require("mongoose");
 
 const messageSchema = new mongoose.Schema({
@@ -12,8 +11,10 @@ const messageSchema = new mongoose.Schema({
     ref: "User",
     required: true
   },
-  content: { type: String, required: true },
-  read: { type: Boolean, default: false }
+  content: {
+    type: String,
+    required: true
+  }
 }, { timestamps: true });
 
 module.exports = mongoose.model("Message", messageSchema);
