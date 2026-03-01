da2bd8ffa36dc6b7a5c5f71fb45784a85819188d
bhumikabanait <bhumikabanait06@gmail.com>
2026-03-01 12:54:07 +0530

    Add authentication middleware for JWT verification

---
+++ Backend/middleware/authMiddleware.js
@@ -0,0 +1,23 @@
+onst jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "No token provided" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = decoded;  // decoded.id must exist
+    next();
+
+  } catch (error) {
+    console.log("JWT ERROR:", error.message);
+    return res.status(401).json({ message: "Invalid token" });
+  }
+};
